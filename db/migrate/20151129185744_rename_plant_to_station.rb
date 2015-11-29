170f01714e787cf998615a3c0f99c643e896ee9e
Michael Ocheje <mocheje@c2gconsulting.com>
2015-11-29 20:43:42 +0100

    completed db migration setup

--- db/migrate/20151129185744_rename_plant_to_station.rb
+++
@@ -1,9 +0,0 @@
-class RenamePlantToStation < ActiveRecord::Migration
-  def self.up
-    rename_table :stations, :stations
-  end
-
-  def self.down
-    rename_table :stations, :stations
-  end
-end
