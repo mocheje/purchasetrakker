aa7944fa73e3d8a3a66890d91acac95acdf2368a
Michael Ocheje <mocheje@c2gconsulting.com>
2016-10-24 09:45:53 +0100

    added host to all environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,6 @@ PurchaseTrakker::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  Rails.application.routes.default_url_options[:host] = 'purchasetrakker.com'
 end
